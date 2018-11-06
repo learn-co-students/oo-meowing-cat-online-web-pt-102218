ae9504bf48d1de14c32ddfc6e2fbf074fbfd27fa
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-11-06 12:44:13 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,27 @@
-## code your solution here. 
+class Cat
+  
+  def initialize(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def meow
+    puts "meow"
+  end
+  
+end
+
+maru = Cat.new
+maru.name = "Maru"
+ 
+maru.name
+ 
+maru.meow
+
